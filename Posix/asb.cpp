fac3470834992c332f355bc04ff9933817fa34c7
byung kyu kim <cdeclare@gmail.com>
2015-03-05 00:06:34 +0900

    - first commit

---
+++ Posix/asb.cpp
@@ -0,0 +1,198 @@
+// asb.cpp
+//
+
+#ifdef _MSC_VER
+ #include "stdafx.h"
+#endif
+
+#include <iostream>
+#include <iomanip>  
+#include <thread>
+#include <future>
+#include <functional>
+#include <mutex>
+using namespace std;
+
+#include "http_client.h"
+#include <boost/format.hpp>
+using boost::str;
+using boost::format;
+
+
+
+void Run(const std::string &url, int connections, int threads, int duration)
+{
+	// connection test
+	{
+		boost::asio::io_service io;
+		http_client client(io);
+		bool b = client.open(url);
+
+		if (!b) {
+			cout << "> Connection error OR Invalid url" << endl;
+			return;
+		}
+	}
+	
+	cout << str(format("> Start and Running %ds (%s)") % duration % http_client::now()) << endl;
+	cout << str(format("    %d connections and %d Threads ")
+		% connections % threads) << endl;
+
+	std::vector<thread> vThread;
+	std::vector<thread> ios;
+
+	std::mutex mtx;
+	vector<shared_ptr<http_client>> vCons;
+	vector<shared_ptr<boost::asio::io_service>> vio;
+
+	auto tm0 = chrono::high_resolution_clock::now();
+	for (int i = 0; i < threads; ++i) {
+
+		auto sio = make_shared<boost::asio::io_service>();
+		vio.push_back(sio);
+
+		thread tr([sio, url, connections, threads, duration, &vCons, &mtx]()
+		{
+			boost::asio::deadline_timer t(*sio, boost::posix_time::seconds(duration));
+			t.async_wait([sio](const boost::system::error_code){
+				sio->stop();
+			});
+
+			int cons = connections / threads;
+			for (int i = 0; i < cons; ++i) {
+				auto c = make_shared<http_client>(*sio);
+				c->open(url);
+				c->async_start();
+
+				{
+					std::lock_guard<std::mutex> lock(mtx);
+					vCons.push_back(c);
+				}
+			}
+			
+			sio->run();
+		});
+
+		vThread.push_back(move(tr));
+	}
+
+	// thread join
+	for (auto &t : vThread) {
+		t.join();
+	}
+
+	auto tm1 = chrono::high_resolution_clock::now();
+	{
+		LogMap logSum;
+		uint64_t request = 0;
+		uint64_t response = 0;
+		uint64_t bytes = 0;
+		uint64_t totial_duration = chrono::duration_cast<ms>(tm1 - tm0).count();
+		
+		for (auto &c : vCons) {
+			for (auto &val : c->get_stat()) {
+				logSum[val.first].request += val.second.request;		// count
+				logSum[val.first].response += val.second.response;		// count
+				logSum[val.first].recv_bytes += val.second.recv_bytes;	// read data size
+			}
+		}
+
+		for (auto &c : logSum) {
+			request += c.second.request;
+			response += c.second.response;
+			bytes += c.second.recv_bytes;
+		}
+
+		auto fnFormat = [](uint64_t size) -> string {
+			double dbytes;
+			string unit = "B";
+			if (size > 1024) {		
+				dbytes = size / 1024.0;
+				unit = "KB";
+			}
+			else if (size > (1024 * 1024)) {
+				dbytes = size / (1024 * 1024.0);
+				unit = "MB";
+			}
+			else if (size > (1024 * 1024 * 1024)) {
+				dbytes = size / (1024 * 1024 * 1024.0);
+				unit = "GB";
+			}
+			else {}
+
+			return str(boost::format("%0.2lf%s") % dbytes % unit);
+		};
+		
+		cout << str(format("> %-22s: %sms") % "Duration" % totial_duration) << endl;
+		cout << str(format("    %-20s: %0.2lfms") % "Latency" % (totial_duration / (double)response)) << endl;
+
+		cout << str(format("    %-20s: %ld") % "Requests Count" % request) << endl;
+		cout << str(format("    %-20s: %ld") % "Response Count" % response) << endl;
+		cout << str(format("    %-20s: %s") % "Response Bytes" % fnFormat(bytes)) << endl;
+		cout << "> Per seconds" << endl;
+		cout << str(format("    %-20s: %0.2lf") % "Requests/sec" % (response / (double)(logSum.size() - 1))) << endl;
+		cout << str(format("    %-20s: %s") % "Response Bytes/sec" % fnFormat(bytes / (logSum.size() - 1))) << endl;
+		cout << endl;
+	}
+
+	vCons.clear();
+}
+
+
+void usage()
+{
+	cout << "Usage: asb <options> <url>" << endl;
+	cout << "  options:" << endl;
+	cout << "    -d <N>    duraction (seconds)" << endl;
+	cout << "    -c <N>    connections" << endl;
+	cout << "    -t <N>    threads" << endl;
+	cout << endl;
+	cout << "  example:    asb -d 10 -c 10 -t 2 http://www.some_url/" << endl;
+	cout << "  version:    0.1" << endl;
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2) {
+		usage();
+		return -1;
+	}
+
+	int connections = 10;
+	int threads = 2;
+	int duration = 10;
+	std::string url;
+
+	try {
+		int i = 1;
+		for (; i < (argc - 1); ++i) {
+			if (string("-d") == argv[i]) {
+				duration = std::stoi(argv[++i]);
+			}
+			else if (string("-c") == argv[i]) {
+				connections = std::stoi(argv[++i]);
+			}
+			else if (string("-t") == argv[i]) {
+				threads = std::stoi(argv[++i]);
+			}
+		}
+
+		if (i != (argc - 1)) throw std::logic_error("Parameter error");
+
+		url = argv[argc - 1];
+	}
+	catch (...) {
+		usage();
+		return -1;
+	}
+	 
+	try {
+		Run(url, connections, threads, duration);
+	}
+	catch (exception &e) {
+		cout << e.what() << endl;
+	}
+
+	return 0;
+}
+
---
+++ Posix/http_client.h
@@ -0,0 +1,322 @@
+
+#pragma once
+
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <string>
+#include <future>
+#include <chrono>
+#include <map>
+#include <regex>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/regex.hpp>
+#include <boost/algorithm/string.hpp>
+
+using boost::asio::ip::tcp;
+
+using ms = std::chrono::milliseconds;
+
+struct http_stat
+{
+	http_stat() : request(0), response(0), recv_bytes(0) {}
+	uint64_t request;
+	uint64_t response;
+	uint64_t recv_bytes;
+};
+
+using LogMap = std::map <string, http_stat>;
+
+class http_client
+{
+public:
+	http_client(boost::asio::io_service& io_service) : resolver_(io_service), socket_(io_service)
+	{
+	}
+
+	~http_client()
+	{
+		close();
+	}
+
+	bool open(const std::string& url)
+	{
+		bool r = false;
+
+		try {
+			if (!urlparser(url, host_, port_, path_)) {
+				throw std::logic_error("error : invalid url");
+			}
+
+			tcp::resolver::query query(host_, port_);
+			auto endpoint_iter = resolver_.resolve(query);
+			boost::asio::connect(socket_, endpoint_iter);
+			r = true;
+		}
+		catch (...) {}
+		return r;
+	}
+
+	void close()
+	{
+		if (is_open()) {
+			socket_.close();
+		}
+	}
+
+	void cancel()
+	{
+		socket_.cancel();
+	}
+
+	void reopen()
+	{
+		if (!is_open()) {
+			tcp::resolver::query query(host_, port_);
+			auto endpoint_iter = resolver_.resolve(query);
+			boost::asio::connect(socket_, endpoint_iter);
+		}
+	}
+
+	void async_start()
+	{
+		async_write();
+		async_read_header();
+	}
+
+	bool is_open()
+	{
+		return socket_.is_open();
+	}
+
+	LogMap& get_stat()
+	{
+		return logm_;
+	}
+
+	static std::string now()
+	{
+		std::time_t now = chrono::system_clock::to_time_t(chrono::system_clock::now());
+		string s = std::ctime(&now);
+		return s.substr(0, s.find_last_of('\n'));
+	}
+
+	static bool urlparser(std::string url, std::string &host, std::string &port, std::string &path)
+	{
+		bool ret = false;
+
+		boost::smatch m;
+		boost::regex r(R"((https?):\/\/([^\/:]*):?([^\/]*)(\/?.*))");
+
+		ret = boost::regex_search(url, m, r);
+
+		if (ret) {
+			host = m[2].str();
+			port = m[3].str();
+			if (port.empty()) port = "80";
+
+			path = m[4].str();
+			if (path.empty()) path = "/";
+		}
+		
+		return ret;
+	}
+
+
+	void async_read_header()
+	{
+		// buuffer initialize 
+		response_.consume(response_.size());
+
+		boost::asio::async_read_until(socket_, response_, "\r\n\r\n",
+			[this /*, t0*/](const boost::system::error_code &err, std::size_t len)
+		{
+			int nRecv = response_.size();
+			// close 
+			if (err) {
+				socket_.shutdown(boost::asio::socket_base::shutdown_both);
+				socket_.close();
+				reopen();
+				async_write();
+				async_read_header();
+			}
+			else {
+				// header invalid check, get content-length 
+				int content_length = parse_header();
+
+				// stastics logging
+				if (content_length >= 0) {
+					//auto dur = chrono::high_resolution_clock::now() - t0_;
+					//auto dcount = chrono::duration_cast<ms>(dur).count();
+
+					std::string sn = now();
+					logm_[sn].response++;
+					logm_[sn].recv_bytes += nRecv;
+				}
+				
+				//cout << "content_length: " << content_length << ", " << "response_.size: " << response_.size();
+				if (content_length > 0 && content_length > (int)response_.size()) {
+					async_read_content(content_length - response_.size());
+				}
+				else {
+					this->async_write();
+					this->async_read_header();
+				}
+			}
+		});
+	}
+
+	void async_read_content(size_t left)
+	{
+		this->response_.consume(this->response_.size());
+
+		//cout << "boost::asio::async_read" << endl;
+		boost::asio::async_read(this->socket_, this->response_, boost::asio::transfer_at_least(1),
+			[this, left](const boost::system::error_code &err, std::size_t len)
+		{
+			//cout << "boost::asio::async_read[](){} - len: " << len << ", err: " << err << endl;
+			if (!err) {
+				std::string sn = now();
+				logm_[sn].recv_bytes += len;
+
+				if (left > len) {
+					async_read_content(left - len);
+				}
+				else {
+					parse_contents();
+
+					this->async_write();
+					this->async_read_header();
+				}
+			}
+			else if (err) {	// err
+				if (err != boost::asio::error::eof) {
+					this->socket_.shutdown(boost::asio::socket_base::shutdown_both);
+					this->socket_.close();
+					this->reopen();
+				}
+				this->async_write();
+				this->async_read_header();
+			}
+		});
+	}
+
+	void async_write()
+	{
+		build_reqeust();
+
+		t0_ = chrono::high_resolution_clock::now();
+		boost::asio::async_write(socket_, request_,
+			[this/*, t0*/](const boost::system::error_code &err, size_t len)
+		{
+			if (!err) {
+				std::string sn = now();
+				logm_[sn].request++;
+
+			}
+			else {
+				this->socket_.shutdown(boost::asio::socket_base::shutdown_both);
+				this->socket_.close();
+			}
+
+		});
+
+	}
+
+private:
+	int parse_header() 
+	{
+		using namespace boost::algorithm;
+		int content_length = 0;
+
+		try {
+			std::istream response_stream(&response_);
+			
+			string header;
+			getline(response_stream, header);
+
+			vector<string> vs;
+			split(vs, header, boost::is_any_of(" "), token_compress_on);
+
+			if (vs.size() < 3) {
+				std::ostringstream oss;
+				oss << "Invalid http hedaer 1 line, vs.size() :  " << vs.size() << endl;
+				throw logic_error(oss.str());
+			}
+
+			string http_version = vs[0];
+			int status_code = std::stoi(vs[1]);
+			string status_msg = vs[2];
+
+			if (!response_stream || http_version.substr(0, 5) != "HTTP/")
+			{
+				throw logic_error("Invalid http header version");
+			}
+
+			if (status_code != 200)
+			{
+				std::ostringstream oss;
+				oss << "Response returned with status code " << status_code << endl;
+				throw logic_error(oss.str());
+			}
+
+			//cout << status_code << endl;
+			while (getline(response_stream, header) && header != "\r") {
+				string::size_type pos = header.find("Content-Length");
+				if (pos != string::npos) {
+					pos = header.find(":");
+					content_length = std::stoi(header.substr(pos + 1));
+				}
+				//cout << header << endl;
+			}
+			
+		}
+		catch (exception &) {
+			content_length = -1;
+			//cout << e.what() << endl;
+		}
+
+		return content_length;
+	}
+
+	bool parse_contents()
+	{
+		try {
+			std::istream response_stream(&response_);
+
+			std::string s;
+			while (!response_stream.eof()) {
+				std::getline(response_stream, s);
+			}
+		}
+		catch (exception &) {
+		}
+
+		return true;
+	}
+
+	void build_reqeust()
+	{
+		this->request_.consume(this->request_.size());
+		std::ostream oss(&request_);
+		oss << "GET http://" << host_ << ":" << port_ << path_ << " HTTP/1.1\r\n";
+		oss << "Host: " << host_ << ":" << port_ << "\r\n";
+		oss << "Accept: */*\r\n";
+		oss << "Connection: keep-alive\r\n";
+		oss << "\r\n";
+	}
+
+private:
+	tcp::resolver resolver_;
+	tcp::socket socket_;
+	boost::asio::streambuf request_;
+	boost::asio::streambuf response_;
+
+	std::string host_;
+	std::string path_;
+	std::string port_;
+
+	LogMap logm_;
+	decltype(chrono::high_resolution_clock::now()) t0_;
+};
