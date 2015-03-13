38b23b1c6e8df70b972d33dac97e2ed7140be498
byung kyu kim <cdeclare@gmail.com>
2015-03-13 12:47:52 +0900

    - compile error, posix

--- targetver.h
+++ targetver.h
@@ -5,4 +5,8 @@
 // 이전 Windows 플랫폼에 대해 응용 프로그램을 빌드하려는 경우에는 SDKDDKVer.h를 포함하기 전에
 // WinSDKVer.h를 포함하고 _WIN32_WINNT 매크로를 지원하려는 플랫폼으로 설정하십시오.
 
-#include <SDKDDKVer.h>
+
+#ifdef _MSC_VER
+ #include <SDKDDKVer.h>
+#endif
+
